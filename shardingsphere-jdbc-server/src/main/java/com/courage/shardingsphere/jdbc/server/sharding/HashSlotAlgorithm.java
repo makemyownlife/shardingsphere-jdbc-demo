0783de2a834d16098b0f7db569a9b169ef5c4a1a
zhangyong <zhangyong7120180@163.com>
2023-02-05 17:53:27 +0800

    [添加代码] Hash 算法

--- shardingsphere-jdbc-server/src/main/java/com/courage/shardingsphere/jdbc/server/sharding/HashSlotAlgorithm.java
+++ shardingsphere-jdbc-server/src/main/java/com/courage/shardingsphere/jdbc/server/sharding/HashSlotAlgorithm.java
@@ -2,16 +2,22 @@ package com.courage.shardingsphere.jdbc.server.sharding;
 
 import org.apache.shardingsphere.api.sharding.complex.ComplexKeysShardingAlgorithm;
 import org.apache.shardingsphere.api.sharding.complex.ComplexKeysShardingValue;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Collection;
 
 /**
- *  复合分片算法
+ * 复合分片算法 :
+ * 主键id 是雪花算法 , workerId = Math.abs(crc32(shardingKey)) % 1024
  */
 public class HashSlotAlgorithm implements ComplexKeysShardingAlgorithm {
 
+    private final static Logger logger = LoggerFactory.getLogger(HashSlotAlgorithm.class);
+
     @Override
     public Collection<String> doSharding(Collection collection, ComplexKeysShardingValue complexKeysShardingValue) {
+        String logicTableName = complexKeysShardingValue.getLogicTableName();
         return null;
     }
 
