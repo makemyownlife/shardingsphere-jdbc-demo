5a96379cb748960e4531b2d63f153ce32af8e0d0
zhangyong <zhangyong7120180@163.com>
2023-02-06 16:12:43 +0800

    [添加代码] application 配置

--- shardingsphere-jdbc-server/src/main/java/com/courage/shardingsphere/jdbc/server/controller/TestController.java
+++ shardingsphere-jdbc-server/src/main/java/com/courage/shardingsphere/jdbc/server/controller/TestController.java
@@ -1,9 +1,12 @@
 package com.courage.shardingsphere.jdbc.server.controller;
 
 import com.courage.shardingsphere.jdbc.common.result.ResponseEntity;
+import com.courage.shardingsphere.jdbc.domain.po.TEntOrder;
+import com.courage.shardingsphere.jdbc.service.OrderService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,10 +17,20 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class TestController {
 
+    @Autowired
+    private OrderService orderService;
+
     @GetMapping("/test")
     @ApiOperation("test")
     public ResponseEntity test() {
         return ResponseEntity.successResult("mylife");
+    }
+
+    @GetMapping("/getOrderById")
+    @ApiOperation("getOrderById")
+    public ResponseEntity getOrderById(String orderId) {
+        TEntOrder tEntOrder = orderService.getOrderById(Long.valueOf(orderId));
+        return ResponseEntity.successResult(tEntOrder);
     }
 
 }
--- shardingsphere-jdbc-service/src/main/java/com/courage/shardingsphere/jdbc/service/OrderService.java
+++ shardingsphere-jdbc-service/src/main/java/com/courage/shardingsphere/jdbc/service/OrderService.java
@@ -1,8 +1,19 @@
 package com.courage.shardingsphere.jdbc.service;
 
+import com.courage.shardingsphere.jdbc.domain.mapper.OrderMapper;
+import com.courage.shardingsphere.jdbc.domain.po.TEntOrder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderService {
+
+    @Autowired
+    private OrderMapper orderMapper;
+
+    public TEntOrder getOrderById(Long id) {
+        return orderMapper.getOrderById(id);
+    }
+
 
 }
