e49c61a8436f8181a948f75436061a606fc97992
zhangyong <zhangyong7120180@163.com>
2023-02-09 09:37:43 +0800

    [添加代码] swagger配置信息

--- shardingsphere-jdbc-server/src/main/java/com/courage/shardingsphere/jdbc/server/config/SwaggerConfig.java
+++ shardingsphere-jdbc-server/src/main/java/com/courage/shardingsphere/jdbc/server/config/SwaggerConfig.java
@@ -3,6 +3,7 @@ package com.courage.shardingsphere.jdbc.server.config;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -13,7 +14,9 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @Configuration
 @EnableSwagger2WebMvc
+@Profile(value = "test")
 public class SwaggerConfig {
+
     @Bean
     public Docket createRestApi() {
         return new Docket(DocumentationType.SWAGGER_2).groupName("应用文档").apiInfo(apiInfo()).select()
