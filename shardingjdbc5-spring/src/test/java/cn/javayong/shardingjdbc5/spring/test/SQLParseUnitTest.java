10b4a580075cd9a4d1895c5950fde7c4ce87e6d4
zhangyong <zhangyong7120180@163.com>
2024-03-25 13:22:07 +0800

    [优化代码] 分库分表 解析引擎 单元测试

---
+++ shardingjdbc5-spring/src/test/java/cn/javayong/shardingjdbc5/spring/test/SQLParseUnitTest.java
@@ -0,0 +1,30 @@
+package cn.javayong.shardingjdbc5.spring.test;
+
+import org.apache.shardingsphere.sql.parser.api.CacheOption;
+import org.apache.shardingsphere.sql.parser.api.SQLParserEngine;
+import org.apache.shardingsphere.sql.parser.api.SQLStatementVisitorEngine;
+import org.apache.shardingsphere.sql.parser.api.visitor.statement.SQLStatementVisitor;
+import org.apache.shardingsphere.sql.parser.core.ParseASTNode;
+import org.apache.shardingsphere.sql.parser.sql.common.statement.SQLStatement;
+import org.junit.Test;
+
+import java.util.Properties;
+
+public class SQLParseUnitTest {
+
+    @Test
+    public void testBase() {
+        boolean useCache = true;
+        String sql = "insert into t_order (order_id,user_id,price) values (?,?,?)";
+        // 获取语法树
+        CacheOption cacheOption = new CacheOption(128, 1024L);
+        SQLParserEngine parserEngine = new SQLParserEngine("MySQL", cacheOption);
+        ParseASTNode parseASTNode = parserEngine.parse(sql, useCache);
+
+        SQLStatementVisitorEngine sqlVisitorEngine = new SQLStatementVisitorEngine("MySQL", true);
+        SQLStatement sqlStatement = sqlVisitorEngine.visit(parseASTNode);
+
+        System.out.println(sqlStatement);
+    }
+
+}
