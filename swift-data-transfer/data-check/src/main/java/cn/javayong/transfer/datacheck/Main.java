1be7bc2853ba3d156ecb9030055873c8a55b8f9b
zhangyong <zhangyong7120180@163.com>
2024-06-15 21:49:15 +0800

    [优化代码] Main 方法

---
+++ swift-data-transfer/data-check/src/main/java/cn/javayong/transfer/datacheck/Main.java
@@ -0,0 +1,10 @@
+package cn.javayong.transfer.datacheck;
+
+/**
+ * Created by zhangyong on 2024/6/15.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
