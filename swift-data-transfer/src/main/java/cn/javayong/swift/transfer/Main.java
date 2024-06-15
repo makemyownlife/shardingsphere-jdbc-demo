fbddbcf94941643a3bb0cf3f7155c37f336668af
zhangyong <zhangyong7120180@163.com>
2024-06-15 18:57:31 +0800

    [优化代码] 新增数据迁移

---
+++ swift-data-transfer/src/main/java/cn/javayong/swift/transfer/Main.java
@@ -0,0 +1,10 @@
+package cn.javayong.swift.transfer;
+
+/**
+ * Created by zhangyong on 2024/6/15.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
