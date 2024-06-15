92e82a4eb82cda20dbd8a4dcd20973ef6bb33b22
zhangyong <zhangyong7120180@163.com>
2024-06-15 21:46:02 +0800

    [优化代码] 新增数据同步模块

--- swift-data-transfer/data-sync/src/main/java/cn/javayong/transfer/datasync/Main.java
+++ swift-data-transfer/data-sync/src/main/java/cn/javayong/transfer/datasync/Main.java
@@ -1,10 +1,12 @@
-package cn.javayong.swift.transfer;
+package cn.javayong.transfer.datasync;
 
 /**
  * Created by zhangyong on 2024/6/15.
  */
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
 }
