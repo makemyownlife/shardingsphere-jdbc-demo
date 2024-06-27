f2804918a41f7e06824942f9682e8e5a63fcd7a8
张勇 <zhangyong7120180@163.com>
2024-06-27 17:37:01 +0800

    [添加代码] 同步组件依赖

--- swift-data-transfer/data-sync/src/main/java/cn/javayong/transfer/datasync/MainApplication.java
+++ swift-data-transfer/data-sync/src/main/java/cn/javayong/transfer/datasync/MainApplication.java
@@ -1,12 +1,18 @@
 package cn.javayong.transfer.datasync;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Created by zhangyong on 2024/6/15.
  */
 public class MainApplication {
 
+    private final static Logger logger = LoggerFactory.getLogger(MainApplication.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
     }
 
 }
