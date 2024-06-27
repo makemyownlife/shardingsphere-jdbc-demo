f49d1586f2e01d88613df969275d6a93f2ca6bb8
zhangyong <zhangyong7120180@163.com>
2024-06-27 17:03:36 +0800

    [优化代码] 数据同步组件 设计

--- swift-data-transfer/data-sync/src/main/java/cn/javayong/transfer/datasync/MainApplication.java
+++ swift-data-transfer/data-sync/src/main/java/cn/javayong/transfer/datasync/MainApplication.java
@@ -3,7 +3,7 @@ package cn.javayong.transfer.datasync;
 /**
  * Created by zhangyong on 2024/6/15.
  */
-public class Main {
+public class MainApplication {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
