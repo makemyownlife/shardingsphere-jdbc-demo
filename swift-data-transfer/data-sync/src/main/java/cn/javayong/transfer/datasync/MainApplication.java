d17f53dffca233bd27505b026a4ce62e38b723f2
张勇 <zhangyong7120180@163.com>
2024-06-27 19:12:02 +0800

    [添加代码]  同步服务主函数

--- swift-data-transfer/data-sync/src/main/java/cn/javayong/transfer/datasync/MainApplication.java
+++ swift-data-transfer/data-sync/src/main/java/cn/javayong/transfer/datasync/MainApplication.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
+ * 同步服务主函数
  * Created by zhangyong on 2024/6/15.
  */
 public class MainApplication {
@@ -11,9 +12,10 @@ public class MainApplication {
     private final static Logger logger = LoggerFactory.getLogger(MainApplication.class);
 
     public static void main(String[] args) {
+        logger.info("开始启动同步服务");
+        long start = System.currentTimeMillis();
 
-
-
+        logger.info("结束启动同步服务 耗时：" + (System.currentTimeMillis() - start) + "毫秒");
     }
 
 }
