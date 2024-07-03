99ea5321004f218b60a9c6549ead33cc6383dc8f
zhangyong <zhangyong7120180@163.com>
2024-07-03 22:35:52 +0800

    [优化] 定义数据源同步配置

--- swift-data-transfer/data-sync/src/main/java/cn/javayong/transfer/datasync/config/DataSyncConfig.java
+++ swift-data-transfer/data-sync/src/main/java/cn/javayong/transfer/datasync/config/DataSyncConfig.java
@@ -1,8 +1,18 @@
 package cn.javayong.transfer.datasync.config;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class DataSyncConfig {
 
+    private HashMap<String, HashMap<String, String>> fullStrategy = new HashMap<String, HashMap<String, String>>();
 
+    public HashMap<String, HashMap<String, String>> getFullStrategy() {
+        return fullStrategy;
+    }
 
+    public void setFullStrategy(HashMap<String, HashMap<String, String>> fullStrategy) {
+        this.fullStrategy = fullStrategy;
+    }
 
 }
