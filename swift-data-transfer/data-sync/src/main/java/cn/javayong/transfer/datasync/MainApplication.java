87e6a00081bebefc1282a8c55b2c7eaafba377c2
张勇 <zhangyong7120180@163.com>
2024-07-04 10:42:53 +0800

    [添加代码] 主函数信息

--- swift-data-transfer/data-sync/src/main/java/cn/javayong/transfer/datasync/MainApplication.java
+++ swift-data-transfer/data-sync/src/main/java/cn/javayong/transfer/datasync/MainApplication.java
@@ -2,6 +2,7 @@ package cn.javayong.transfer.datasync;
 
 import cn.javayong.transfer.datasync.config.DataSyncConfig;
 import cn.javayong.transfer.datasync.support.YamlLoader;
+import com.alibaba.fastjson.JSON;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,10 +20,9 @@ public class MainApplication {
 
         // 加载配置
         DataSyncConfig dataSyncConfig = YamlLoader.loadConfig();
+        logger.info("dataSyncConfig:" + JSON.toJSONString(dataSyncConfig));
 
-        // 配置全量
 
-        // 配置增量
 
         logger.info("结束启动同步服务 耗时：" + (System.currentTimeMillis() - start) + "毫秒");
     }
