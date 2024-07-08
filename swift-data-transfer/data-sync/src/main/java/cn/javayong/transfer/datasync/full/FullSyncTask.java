0539fc1772f7893a44ab9d9bfa9535aa684e692e
zhangyong <zhangyong7120180@163.com>
2024-07-08 21:58:17 +0800

    启动全量同步任务线程

--- swift-data-transfer/data-sync/src/main/java/cn/javayong/transfer/datasync/full/FullSyncTask.java
+++ swift-data-transfer/data-sync/src/main/java/cn/javayong/transfer/datasync/full/FullSyncTask.java
@@ -1,8 +1,11 @@
 package cn.javayong.transfer.datasync.full;
 
+import com.alibaba.druid.pool.DruidDataSource;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,20 +18,71 @@ public class FullSyncTask {
 
     private HashMap<String, HashMap<String, Object>> fullStrategy;
 
+    private DruidDataSource sourceDataSource;
+
+    private DruidDataSource targetDataSource;
+
+    private Thread executeThread;
+
     public FullSyncTask(HashMap<String, HashMap<String, Object>> fullStrategy) {
         this.fullStrategy = fullStrategy;
     }
 
     public void start() {
         Map<String, Object> tableConfig = fullStrategy.get("tableConfig"); // 同步表配置
+        // 解析数据源参数
+        HashMap<String, Object> sourceMap = fullStrategy.get("source");  //  源数据库
+        HashMap<String, Object> targetMap = fullStrategy.get("target");  //  目的数据库
 
-        // 解析参数，开启独立的线程执行任务
+        // 初始化数据源连接池
+        this.sourceDataSource = initDataSource(sourceMap);
+        this.targetDataSource = initDataSource(targetMap);
 
+        // 开启独立的线程执行任务
+        this.executeThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                process();
+            }
+        });
+        this.executeThread.setName("fullExecuteThread");
+        this.executeThread.start();
+    }
 
+    private void process() {
+        while (true) {
+            logger.info("processing");
+            try {
+                Thread.sleep(2000);
+            } catch (Exception e) {
+            }
+        }
+    }
+
+    private DruidDataSource initDataSource(HashMap<String, Object> map) {
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setUrl(String.valueOf(map.get("url")));
+        dataSource.setUsername(String.valueOf(map.get("username")));
+        dataSource.setUsername(String.valueOf(map.get("password")));
+        dataSource.setMinIdle(1);
+        dataSource.setMaxActive(30);
+        try {
+            dataSource.init();
+        } catch (Exception e) {
+            logger.error("init error:", e);
+        }
+        return dataSource;
     }
 
     public void stop() {
+        //
 
+        if (sourceDataSource != null) {
+            sourceDataSource.close();
+        }
+        if (targetDataSource != null) {
+            targetDataSource.close();
+        }
     }
 
 }
