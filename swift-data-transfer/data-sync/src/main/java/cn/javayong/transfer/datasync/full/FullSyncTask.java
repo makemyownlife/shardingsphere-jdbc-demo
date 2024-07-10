766d7d17fb58bd2f54e9799548e4bcb0f521afd0
zhangyong <zhangyong7120180@163.com>
2024-07-10 22:01:49 +0800

    [优化代码] 获取 元数据源

--- swift-data-transfer/data-sync/src/main/java/cn/javayong/transfer/datasync/full/FullSyncTask.java
+++ swift-data-transfer/data-sync/src/main/java/cn/javayong/transfer/datasync/full/FullSyncTask.java
@@ -1,14 +1,10 @@
 package cn.javayong.transfer.datasync.full;
 
 import com.alibaba.druid.pool.DruidDataSource;
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -53,18 +49,33 @@ public class FullSyncTask {
     }
 
     private void process() {
-        while (true) {
-            try {
-                // 读取当前最小 id
-                Connection sourceConnection = sourceDataSource.getConnection();
-                DatabaseMetaData SourceMetaData = sourceConnection.getMetaData();
+        try {
+            // 读取当前最小 id
+            Connection sourceConnection = sourceDataSource.getConnection();
+            DatabaseMetaData SourceMetaData = sourceConnection.getMetaData();
 
-                String tableName = "your_table";
-                ResultSet columnsResultRs = SourceMetaData.getColumns(null, null, tableName, null);
-                
 
-            } catch (Exception e) {
+            String tableName = "t_ent_order";
+            ResultSet columns = SourceMetaData.getColumns(
+                    null,
+                    null,
+                    tableName,
+                    null
+            );
+
+            // 4. 遍历结果集并输出列信息
+            while (columns.next()) {
+                String columnName = columns.getString("COLUMN_NAME");
+                String columnType = columns.getString("TYPE_NAME");
+                int dataType = columns.getInt("DATA_TYPE");
+
+                // 将 SQL 类型代码转换为类型名称
+                String typeName = getTypeName(dataType);
+
+                System.out.println("Column Name: " + columnName + ", Type: " + columnType + ", SQL Type: " + typeName);
             }
+
+        } catch (Exception e) {
         }
     }
 
@@ -92,6 +103,84 @@ public class FullSyncTask {
         if (targetDataSource != null) {
             targetDataSource.close();
         }
+    }
+
+    private static String getTypeName(int dataType) {
+        switch (dataType) {
+            case Types.VARCHAR:
+                return "VARCHAR";
+            case Types.CHAR:
+                return "CHAR";
+            case Types.LONGVARCHAR:
+                return "LONGVARCHAR";
+            case Types.NUMERIC:
+                return "NUMERIC";
+            case Types.DECIMAL:
+                return "DECIMAL";
+            case Types.BIT:
+                return "BIT";
+            case Types.TINYINT:
+                return "TINYINT";
+            case Types.SMALLINT:
+                return "SMALLINT";
+            case Types.INTEGER:
+                return "INTEGER";
+            case Types.BIGINT:
+                return "BIGINT";
+            case Types.REAL:
+                return "REAL";
+            case Types.FLOAT:
+                return "FLOAT";
+            case Types.DOUBLE:
+                return "DOUBLE";
+            case Types.BINARY:
+                return "BINARY";
+            case Types.VARBINARY:
+                return "VARBINARY";
+            case Types.LONGVARBINARY:
+                return "LONGVARBINARY";
+            case Types.DATE:
+                return "DATE";
+            case Types.TIME:
+                return "TIME";
+            case Types.TIMESTAMP:
+                return "TIMESTAMP";
+            case Types.CLOB:
+                return "CLOB";
+            case Types.BLOB:
+                return "BLOB";
+            case Types.ARRAY:
+                return "ARRAY";
+            case Types.STRUCT:
+                return "STRUCT";
+            case Types.REF:
+                return "REF";
+            case Types.DATALINK:
+                return "DATALINK";
+            case Types.BOOLEAN:
+                return "BOOLEAN";
+            case Types.ROWID:
+                return "ROWID";
+            case Types.NCHAR:
+                return "NCHAR";
+            case Types.NVARCHAR:
+                return "NVARCHAR";
+            case Types.LONGNVARCHAR:
+                return "LONGNVARCHAR";
+            case Types.NCLOB:
+                return "NCLOB";
+            case Types.SQLXML:
+                return "SQLXML";
+            case Types.REF_CURSOR:
+                return "REF_CURSOR";
+            case Types.TIME_WITH_TIMEZONE:
+                return "TIME_WITH_TIMEZONE";
+            case Types.TIMESTAMP_WITH_TIMEZONE:
+                return "TIMESTAMP_WITH_TIMEZONE";
+            default:
+                return "UNKNOWN";
+        }
+
     }
 
 }
